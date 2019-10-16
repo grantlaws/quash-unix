c3bc0a876ded78e42cb1b017bf2cd045778f67e2
bailey srimoungchanh <b756s748@ku.edu>
2019-10-16 07:31:50 -0500

    Added code to display envp

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "cmdline.h"
 #include "redirec.h"
+#include <string.h>
 
 int main(int argc, char** argv, char** envp)
 {
+    if(argc > 1 && strcmp(argv[1],"env") == 0){
+        for(int i = 0; i < strlen(*envp); i++)
+        {
+            printf("%s\n", envp[i]);
+        }
+    }
 	if(argc == 1) //Command Line
 	{
 		cmdline_exec();
